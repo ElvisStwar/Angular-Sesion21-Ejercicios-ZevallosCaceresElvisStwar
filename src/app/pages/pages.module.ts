c98c4b001a9d907df19b9bd9b75af93bcc8129b7
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-25 11:28:37 -0500

    creacion de modulos y componenetes

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { PostComponent } from './post/post.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    PostComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
