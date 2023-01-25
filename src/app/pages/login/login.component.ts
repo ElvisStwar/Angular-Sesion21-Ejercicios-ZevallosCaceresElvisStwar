b6d79b1a7e68cd2e2f7eaca346445320f26856f9
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-25 12:44:21 -0500

    validacion de forms login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  email!:string;
+  password!:string;
+  mensajeCorrecto=false
+  mensajeIncorrecto=false
+
+  public formLogin!: FormGroup;
+
+  constructor(private formBuilder:FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+      
+    this.formLogin = this.formBuilder.group({
+      email: ['',
+    [
+      Validators.required,
+    ]],
+
+      password: ['',
+      Validators.required
+    ]
+    })
+    console.log(this.formLogin.valid)
+  }
+  CamposCorrectos = false;
+  send():any{
+    this.CamposCorrectos = this.formLogin.valid;
+    if(this.email=="AD"||this.password=="123456"){
+      this.mensajeCorrecto=true;
+      this.mensajeIncorrecto=false;
+    }else{
+      this.mensajeIncorrecto=true;
+      this.mensajeCorrecto=false;
+    }
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { PostComponent } from './post/post.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -13,7 +13,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   exports: [
     LoginComponent,
