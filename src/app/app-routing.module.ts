6eef47afbb812e29211a106e4ecfc8f1ad2df4c3
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-25 11:31:31 -0500

    creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { PostComponent } from './pages/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"", component:LoginComponent},
+  {path:"login", component:LoginComponent},
+  {path:"post", component:PostComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
